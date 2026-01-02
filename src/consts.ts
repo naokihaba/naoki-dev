43f63ac964ce128f4c72bfdd1c240b6b1a450e14
naokihaba <naosprintrunner6385@gmail.com>
2026-01-02 20:36:14 +0900

    feat: update site title with emoji and add note about hatena bookmark comments

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "naoki.dev";
+export const SITE_TITLE = "🚀 naoki.dev";
 export const SITE_DESCRIPTION = "NaokiHaba's personal blog";
